b91337582c82ce838447d7eb058a72a16a1ca01d
otsaharm <sahar.oy@hotmail.com>
2025-04-26 19:37:23 +0300

    Add files via upload

---
+++ MyGame/MyGame/MyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGameApp.swift
+//  MyGame
+//
+//  Created by Sahar Otiyn on 28/10/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct MyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
